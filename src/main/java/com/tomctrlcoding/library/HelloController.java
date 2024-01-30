5b26488c080485be9326b71ce73e87cb2a81d0e1
tomvanp <tomvanpoucke.1@gmail.com>
2024-01-30 10:25:49 +0530

    Base project generated by start.microprofile.io

---
+++ src/main/java/com/tomctrlcoding/library/HelloController.java
@@ -0,0 +1,19 @@
+package com.tomctrlcoding.library;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/tomctrlcoding/library/LibraryRestApplication.java
@@ -0,0 +1,12 @@
+package com.tomctrlcoding.library;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class LibraryRestApplication extends Application {
+}
